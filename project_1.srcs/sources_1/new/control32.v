5942f37a85b0aca102beee25db7bfd8af355ef15
KouHiiChan <kohichan8@gmail.com>
2023-05-24 05:57:54 +0800

    repair ALUOp and Alu_result_high mistake

--- project_1.srcs/sources_1/new/control32.v
+++ project_1.srcs/sources_1/new/control32.v
@@ -42,7 +42,7 @@ output IORead,
 output IOWrite
     );
 
-output[1:0] ALUOp;
+wire[1:0] ALUOp;
 
 wire R_format, Lw, Sw;
 assign R_format = (Opcode==6'b000000) ? 1'b1:1'b0; 
@@ -61,7 +61,7 @@ assign I_format = (Opcode[5:3]==3'b001) ? 1'b1:1'b0;
 assign ALUSrc = (I_format || Lw || Sw) ? 1'b1 : 1'b0;
 assign ALUOp = {(R_format || I_format),(Branch || nBranch)};
 assign Sftmd = (((Function_opcode==6'b000000)||(Function_opcode==6'b000010) ||(Function_opcode==6'b000011)||(Function_opcode==6'b000100) ||(Function_opcode==6'b000110)||(Function_opcode==6'b000111)) && R_format)? 1'b1:1'b0;
-assign MemRead = ((Lw==1)&&(Alu_resultHigh[21:0]!=22'h3FFFFF))?1'b1:1'b0;
-assign IORead = ((Lw==1)&&(Alu_resultHigh[21:0]==22'h3FFFFF))?1'b1:1'b0;
-assign IOWrite = ((Sw==1)&&(Alu_resultHigh[21:0]==22'h3FFFFF))?1'b1:1'b0;
+assign MemRead = ((Lw==1)&&(ALU_result_high[21:0]!=22'h3FFFFF))?1'b1:1'b0;
+assign IORead = ((Lw==1)&&(ALU_result_high[21:0]==22'h3FFFFF))?1'b1:1'b0;
+assign IOWrite = ((Sw==1)&&(ALU_result_high[21:0]==22'h3FFFFF))?1'b1:1'b0;
 endmodule
